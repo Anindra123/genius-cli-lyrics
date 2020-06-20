c01b360f52fa22c270c3f7aa9941a2ecb25d503e
Anindra <bivasdas911@gmail.com>
2020-06-21 00:33:49 +0600

    Added script and initialized a README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# genius-lyrics-cli
+
+A python cli client that uses [Genius Api](https://genius.com/developers) and [requests_html](https://requests-html.kennethreitz.org/)library for scraping to get the lyrics of a song on the command line
+
+
+you need an authoriztion key which can be get through creating a free account at [Genius](https://genius.com/) then setting it as an environment variable on your ```.bashrc``` or ```.zshrc``` file like
+
+``````
+export GENIUS_ACCESS_TOKEN="your token"
+
+``````
+
+## Build
+
+``````
+clone the repository
+cd genius-lyrics-cli/
+chown u+x lyrics.py
+
+``````
+
+Then run the script to get the desried lyrics of a song on the command line
+
+``````
+./lyrics.py Artist_name Song_name
+
+``````
+
+## Dependecies
+
+* [requests_html](https://requests-html.kennethreitz.org/)
+* [argparse](https://requests-html.kennethreitz.org/)
+* Python 
+* pip
+
+## Credits
+* Genius Api(https://genius.com/developers)
+* Genius Website (https://genius.com/)
---
+++ lyrics.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+import  requests
+import argparse
+import os
+from requests_html import HTML,HTMLSession
+
+'''
+Credits : 
+Genius Api(https://genius.com/developers)
+Genius Website (https://genius.com/)
+'''
+
+
+
+
+
+
+#Getting command line arguments
+parse = argparse.ArgumentParser(prog="lyrics.py",usage='%(prog)s [Artist Name] [Song Name]')
+parse.add_argument("Artist_and_Song_name",nargs='+',help="Get the names of the song and artist")
+args = parse.parse_args()
+
+#Setting artist name and song
+artist_and_song = ' '.join(args.Artist_and_Song_name)
+
+#Setting the url for request
+base_url = 'https://api.genius.com' 
+access_token = os.environ.get('GENIUS_ACCESS_TOKEN') 
+param= {'q':artist_and_song}
+path = '/search'
+token = 'Bearer '+access_token
+header = {'Authorization': token}
+
+request = requests.get(base_url+path, params=param, headers=header)
+
+#Checking Request Status
+if(request.ok):
+   r_dict = request.json()
+else:
+    print("Bad Request")
+
+#Getting the title of the song and genius lyrics url
+response = r_dict['response']['hits'][0]['result']
+title = response['full_title']
+url = response['url']
+print(title)
+print(url)
+
+#Setting up requests_html library for scraping the website
+session = HTMLSession()
+r = session.get(url)
+print(r.status_code)
+
+#Getting the first html element containing  a <p> tag
+song_body = r.html.find("p",first=True).text
+print(song_body)
